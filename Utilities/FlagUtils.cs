c85dce041b7f3fbc183a04a46e2553f4e0438a28
Kryzarel <8752418+Kryzarel@users.noreply.github.com>
2024-11-30 00:15:22 +0000

    FlagUtils change check to != 0

--- Utilities/FlagUtils.cs
+++ Utilities/FlagUtils.cs
@@ -4,37 +4,37 @@ namespace Kryz.Utils
 	{
 		public static bool HasFlag(this byte mask, byte flag)
 		{
-			return (mask & flag) == flag;
+			return (mask & flag) != 0;
 		}
 
 		public static bool HasFlag(this short mask, short flag)
 		{
-			return (mask & flag) == flag;
+			return (mask & flag) != 0;
 		}
 
 		public static bool HasFlag(this ushort mask, ushort flag)
 		{
-			return (mask & flag) == flag;
+			return (mask & flag) != 0;
 		}
 
 		public static bool HasFlag(this int mask, int flag)
 		{
-			return (mask & flag) == flag;
+			return (mask & flag) != 0;
 		}
 
 		public static bool HasFlag(this uint mask, uint flag)
 		{
-			return (mask & flag) == flag;
+			return (mask & flag) != 0;
 		}
 
 		public static bool HasFlag(this long mask, long flag)
 		{
-			return (mask & flag) == flag;
+			return (mask & flag) != 0;
 		}
 
 		public static bool HasFlag(this ulong mask, ulong flag)
 		{
-			return (mask & flag) == flag;
+			return (mask & flag) != 0;
 		}
 	}
 }
