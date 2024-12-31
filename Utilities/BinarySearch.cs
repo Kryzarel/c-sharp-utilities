1f4c6e5ec6f7c42311ed6ac257bb3f656b3cc93e
Kryzarel <8752418+Kryzarel@users.noreply.github.com>
2024-12-31 15:20:27 +0000

    Improved binary search generics

--- Utilities/BinarySearch.cs
+++ Utilities/BinarySearch.cs
@@ -1,17 +1,30 @@
-using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace Kryz.Utils
 {
 	public static class BinarySearch
 	{
-		public static int BinarySearchLeftmost<T>(this IReadOnlyList<T> list, T value) => BinarySearchLeftmost(list, value, 0, list.Count);
-
-		public static int BinarySearchLeftmost<T>(this IReadOnlyList<T> list, T value, int count) => BinarySearchLeftmost(list, value, 0, count);
-
-		public static int BinarySearchLeftmost<T>(this IReadOnlyList<T> list, T value, int index, int count, IComparer<T>? comparer = null)
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static int BinarySearchLeftmost<T, TList, TComp>(this TList list, T value) where TList : IReadOnlyList<T> where TComp : IComparer<T>
 		{
-			comparer ??= Comparer<T>.Default;
+			return list.BinarySearchLeftmost(value, 0, list.Count, Comparer<T>.Default);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static int BinarySearchLeftmost<T, TList, TComp>(this TList list, T value, int index, int count) where TList : IReadOnlyList<T> where TComp : IComparer<T>
+		{
+			return list.BinarySearchLeftmost(value, index, count, Comparer<T>.Default);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static int BinarySearchLeftmost<T, TList, TComp>(this TList list, T value, TComp comparer) where TList : IReadOnlyList<T> where TComp : IComparer<T>
+		{
+			return list.BinarySearchLeftmost(value, 0, list.Count, comparer);
+		}
+
+		public static int BinarySearchLeftmost<T, TList, TComp>(this TList list, T value, int index, int count, TComp comparer) where TList : IReadOnlyList<T> where TComp : IComparer<T>
+		{
 			int min = index;
 			int max = count;
 
@@ -20,25 +33,6 @@ namespace Kryz.Utils
 				int mid = (min + max) / 2;
 
 				if (comparer.Compare(list[mid], value) < 0)
-					min = mid + 1;
-				else
-					max = mid;
-			}
-			return min;
-		}
-
-		public static int BinarySearchLeftmost<T>(this IReadOnlyList<T> list, Func<T, int> predicate) => BinarySearchLeftmost(list, 0, list.Count, predicate);
-
-		public static int BinarySearchLeftmost<T>(this IReadOnlyList<T> list, int index, int count, Func<T, int> predicate)
-		{
-			int min = index;
-			int max = count;
-
-			while (min < max)
-			{
-				int mid = (min + max) / 2;
-
-				if (predicate(list[mid]) < 0)
 					min = mid + 1;
 				else
 					max = mid;
