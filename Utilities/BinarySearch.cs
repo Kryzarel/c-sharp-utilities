7aa12cd7603c97e9a4bbc35ced7f7b69698d8fb0
Kryzarel <8752418+Kryzarel@users.noreply.github.com>
2024-12-31 17:40:18 +0000

    Modified BinarySearch overloads for more easily inferred generic parameters

--- Utilities/BinarySearch.cs
+++ Utilities/BinarySearch.cs
@@ -6,13 +6,13 @@ namespace Kryz.Utils
 	public static class BinarySearch
 	{
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static int BinarySearchLeftmost<T, TList, TComp>(this TList list, T value) where TList : IReadOnlyList<T> where TComp : IComparer<T>
+		public static int BinarySearchLeftmost<T, TList>(this TList list, T value) where TList : IReadOnlyList<T>
 		{
 			return list.BinarySearchLeftmost(value, 0, list.Count, Comparer<T>.Default);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static int BinarySearchLeftmost<T, TList, TComp>(this TList list, T value, int index, int count) where TList : IReadOnlyList<T> where TComp : IComparer<T>
+		public static int BinarySearchLeftmost<T, TList>(this TList list, T value, int index, int count) where TList : IReadOnlyList<T>
 		{
 			return list.BinarySearchLeftmost(value, index, count, Comparer<T>.Default);
 		}
