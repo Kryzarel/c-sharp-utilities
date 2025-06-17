2852817e3f26aa9ccf8ad09bace8b26ea10040a5
Kryzarel <8752418+Kryzarel@users.noreply.github.com>
2025-06-17 23:50:20 +0100

    Changed BinarySearchExtensions to use equivalent to C#'s own BinarySearch

--- Utilities/BinarySearchExtensions.cs
+++ Utilities/BinarySearchExtensions.cs
@@ -6,38 +6,41 @@ namespace Kryz.Utils
 	public static class BinarySearchExtensions
 	{
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static int BinarySearchLeftmost<T, TList>(this TList list, T value) where TList : IReadOnlyList<T>
+		public static int BinarySearch<T, TList>(this TList list, T value) where TList : IReadOnlyList<T>
 		{
-			return list.BinarySearchLeftmost(value, 0, list.Count, Comparer<T>.Default);
+			return list.BinarySearch(value, 0, list.Count, Comparer<T>.Default);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static int BinarySearchLeftmost<T, TList>(this TList list, T value, int index, int count) where TList : IReadOnlyList<T>
+		public static int BinarySearch<T, TList>(this TList list, T value, int index, int count) where TList : IReadOnlyList<T>
 		{
-			return list.BinarySearchLeftmost(value, index, count, Comparer<T>.Default);
+			return list.BinarySearch(value, index, count, Comparer<T>.Default);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static int BinarySearchLeftmost<T, TList, TComp>(this TList list, T value, TComp comparer) where TList : IReadOnlyList<T> where TComp : IComparer<T>
+		public static int BinarySearch<T, TList, TComp>(this TList list, T value, TComp comparer) where TList : IReadOnlyList<T> where TComp : IComparer<T>
 		{
-			return list.BinarySearchLeftmost(value, 0, list.Count, comparer);
+			return list.BinarySearch(value, 0, list.Count, comparer);
 		}
 
-		public static int BinarySearchLeftmost<T, TList, TComp>(this TList list, T value, int index, int count, TComp comparer) where TList : IReadOnlyList<T> where TComp : IComparer<T>
+		public static int BinarySearch<T, TList, TComp>(this TList list, T value, int index, int count, TComp comparer) where TList : IReadOnlyList<T> where TComp : IComparer<T>
 		{
 			int min = index;
-			int max = count;
+			int max = index + count - 1;
 
-			while (min < max)
+			while (min <= max)
 			{
-				int mid = (min + max) / 2;
+				int mid = min + ((max - min) >> 1);
 
-				if (comparer.Compare(list[mid], value) < 0)
+				int result = comparer.Compare(list[mid], value);
+				if (result == 0) return mid;
+
+				if (result < 0)
 					min = mid + 1;
 				else
-					max = mid;
+					max = mid - 1;
 			}
-			return min;
+			return ~min;
 		}
 	}
 }
