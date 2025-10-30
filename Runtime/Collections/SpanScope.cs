e0893edf3d999b3b089c71cae608fdc35f0b1655
Kryzarel <8752418+Kryzarel@users.noreply.github.com>
2025-10-30 19:03:26 +0000

    Added Summary comment to SpanScope

--- Runtime/Collections/SpanScope.cs
+++ Runtime/Collections/SpanScope.cs
@@ -3,6 +3,15 @@ using System.Buffers;
 
 namespace Kryz.Utils
 {
+	/// <summary>
+	/// <para>When working with stackalloc, it's common to write code like this:</para>
+	/// <para><see cref="Span{T}"/> buffer = size > 1024 ? new T[size] : stackallock T[size]; </para>
+	/// <para>But if we want to use <see cref="ArrayPool{T}"/> instead of new T[], we need to keep track of the rented array, clear it and return it to the pool.</para>
+	/// <para>With SpanScope we can reduce all of that to a single line, just like with new T[]:</para>
+	/// <para><see cref="using"/> <see cref="SpanScope{T}"/> scope = size > 1024 ? new <see cref="SpanScope{T}"/>(size) : stackallock T[size];</para>
+	/// <para>Since it implements the Dispose pattern, declaring a <see cref="SpanScope{T}"/> variable with the '<see cref="using"/>' keyword causes the Dispose()
+	/// method to be automatically called when the variable goes out of scope, which clears and returns the array.</para>
+	/// </summary>
 	public readonly ref struct SpanScope<T>
 	{
 		public readonly Span<T> Span;
