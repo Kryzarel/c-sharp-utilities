4b18f6f070b963e3ce52aa21ea8876e90ed4009d
Kryzarel <8752418+Kryzarel@users.noreply.github.com>
2026-02-04 22:42:36 +0000

    Renamed SpanScope to PooledSpan. Added ValuePooledList.

--- Runtime/Collections/PooledSpan.cs
+++ Runtime/Collections/PooledSpan.cs
@@ -7,12 +7,12 @@ namespace Kryz.Utils
 	/// <para>When working with stackalloc, it's common to write code like this:</para>
 	/// <para><see cref="Span{T}"/> buffer = size > 1024 ? new T[size] : stackallock T[size]; </para>
 	/// <para>But if we want to use <see cref="ArrayPool{T}"/> instead of new T[], we need to keep track of the rented array, clear it and return it to the pool.</para>
-	/// <para>With SpanScope we can reduce all of that to a single line, just like with new T[]:</para>
-	/// <para><see cref="using"/> <see cref="SpanScope{T}"/> scope = size > 1024 ? new <see cref="SpanScope{T}"/>(size) : stackallock T[size];</para>
-	/// <para>Since it implements the Dispose pattern, declaring a <see cref="SpanScope{T}"/> variable with the '<see cref="using"/>' keyword causes the Dispose()
-	/// method to be automatically called when the variable goes out of scope, which clears and returns the array.</para>
+	/// <para>With <see cref="PooledSpan{T}"/> we can reduce all of that to a single line, just like with new T[]:</para>
+	/// <para><see cref="using"/> <see cref="PooledSpan{T}"/> buffer = size > 1024 ? new <see cref="PooledSpan{T}"/>(size) : stackallock T[size];</para>
+	/// <para>Since it implements the Dispose pattern, declaring a <see cref="PooledSpan{T}"/> with the '<see cref="using"/>' keyword, automatically calls Dispose()
+	/// when the variable goes out of scope, which clears and returns the rented array.</para>
 	/// </summary>
-	public readonly ref struct SpanScope<T>
+	public readonly ref struct PooledSpan<T>
 	{
 		public ref T this[int index] => ref Span[index];
 
@@ -22,7 +22,7 @@ namespace Kryz.Utils
 		private readonly T[]? array;
 		private readonly ArrayPool<T>? pool;
 
-		public SpanScope(int size, ArrayPool<T>? arrayPool = null)
+		public PooledSpan(int size, ArrayPool<T>? arrayPool = null)
 		{
 			pool = arrayPool ?? ArrayPool<T>.Shared;
 			array = pool.Rent(size);
@@ -30,7 +30,7 @@ namespace Kryz.Utils
 			Length = Span.Length;
 		}
 
-		private SpanScope(Span<T> span)
+		private PooledSpan(Span<T> span)
 		{
 			Span = span;
 			array = null;
@@ -47,7 +47,7 @@ namespace Kryz.Utils
 			}
 		}
 
-		public static implicit operator SpanScope<T>(Span<T> span) => new(span);
-		public static implicit operator Span<T>(SpanScope<T> scope) => scope.Span;
+		public static implicit operator PooledSpan<T>(Span<T> span) => new(span);
+		public static implicit operator Span<T>(PooledSpan<T> pooled) => pooled.Span;
 	}
 }
---
+++ Runtime/Collections/ValuePooledList.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Buffers;
+
+namespace Kryz.Utils
+{
+	public ref struct ValuePooledList<T>
+	{
+		private ArrayPool<T>? arrayPool;
+
+		private Span<T> span;
+		private T[]? array;
+		private int count;
+
+		public readonly int Count => count;
+		public readonly int Capacity => span.Length;
+
+		public readonly Span<T> AsSpan() => span[..count];
+
+		public readonly ref T this[int i] => ref span[..count][i];
+
+		public ValuePooledList(Span<T> initialSpan)
+		{
+			arrayPool = ArrayPool<T>.Shared;
+			span = initialSpan;
+			array = null;
+			count = 0;
+		}
+
+		public ValuePooledList(int capacity, ArrayPool<T>? pool = null)
+		{
+			arrayPool = pool ?? ArrayPool<T>.Shared;
+			array = arrayPool.Rent(capacity);
+			span = array;
+			count = 0;
+		}
+
+		public void Add(T item)
+		{
+			if (count == span.Length)
+				Grow();
+
+			span[count++] = item;
+		}
+
+		public ref T Add()
+		{
+			if (count == span.Length)
+				Grow();
+
+			return ref span[count++];
+		}
+
+		public void AddRange(ReadOnlySpan<T> items)
+		{
+			if (count + items.Length > span.Length)
+				Grow(count + items.Length);
+
+			items.CopyTo(span[count..]);
+			count += items.Length;
+		}
+
+		public void Clear()
+		{
+			count = 0;
+		}
+
+		private void Grow(int requiredCapacity = 0)
+		{
+			int newCapacity = Math.Max(requiredCapacity, span.Length == 0 ? 4 : span.Length * 2);
+
+			T[]? oldArray = array;
+
+			arrayPool ??= ArrayPool<T>.Shared;
+			array = arrayPool.Rent(newCapacity);
+			span[..count].CopyTo(array);
+			span = array;
+
+			if (oldArray != null)
+			{
+				Array.Clear(oldArray, 0, count);
+				arrayPool.Return(oldArray);
+			}
+		}
+
+		public void Dispose()
+		{
+			if (array != null)
+			{
+				Array.Clear(array, 0, count);
+				arrayPool ??= ArrayPool<T>.Shared;
+				arrayPool.Return(array);
+				array = null;
+			}
+		}
+
+		public static implicit operator Span<T>(ValuePooledList<T> pooled) => pooled.AsSpan();
+	}
+}
