314c115723e786e918deabdef3bfb516282f02a8
Kryzarel <8752418+Kryzarel@users.noreply.github.com>
2026-02-26 01:18:50 +0000

    Added missing Array and Span 'Search' methods in BinarySearch

--- Runtime/Search/BinarySearch.cs
+++ Runtime/Search/BinarySearch.cs
@@ -5,6 +5,34 @@ namespace Kryz.Utils
 {
 	public static class BinarySearch
 	{
+		// Array overloads allow us to use this class as a drop-in replacement for Array.BinarySearch
+		public static int Search<T>(T[] data, T value) => Search(data.AsSpan(), value, Comparer<T>.Default);
+		public static int Search<T>(T[] data, int index, int length, T value) => Search(data.AsSpan(index, length), value, Comparer<T>.Default);
+		public static int Search<T, TComparer>(T[] data, T value, TComparer comparer) where TComparer : IComparer<T> => Search(data.AsSpan(), value, comparer);
+		public static int Search<T, TComparer>(T[] data, int index, int length, T value, TComparer comparer) where TComparer : IComparer<T> => Search(data.AsSpan(index, length), value, comparer);
+
+		public static int Search<T>(Span<T> data, T value) => Search(data, value, Comparer<T>.Default);
+
+		public static int Search<T, TComparer>(Span<T> data, T value, TComparer comparer) where TComparer : IComparer<T>
+		{
+			int min = 0;
+			int max = data.Length - 1;
+
+			while (min <= max)
+			{
+				int mid = min + ((max - min) >> 1);
+				int result = comparer.Compare(data[mid], value);
+
+				if (result == 0)
+					return mid;
+				else if (result < 0)
+					min = mid + 1;
+				else
+					max = mid - 1;
+			}
+			return ~min;
+		}
+
 		public static int Search<T>(IList<T> data, T value) => Search(data, 0, data.Count, value, Comparer<T>.Default);
 		public static int Search<T>(IList<T> data, int index, int length, T value) => Search(data, index, length, value, Comparer<T>.Default);
 		public static int Search<T, TComparer>(IList<T> data, T value, TComparer comparer) where TComparer : IComparer<T> => Search(data, 0, data.Count, value, comparer);
